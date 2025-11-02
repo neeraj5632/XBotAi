09e5c69caf69a2260b5e40c100ac2f51b6676f4b
Neeraj <neerajpande08@gmail.com>
2025-11-03 00:28:06 +0530

    final is shoud work

--- src/data/sampleData.jsx
+++ src/data/sampleData.jsx
@@ -2,7 +2,7 @@ const sampleData = {
   "Hi, what is the weather": "The weather is sunny and pleasant today!",
   "Hi, what is my location": "You are currently in your city.",
   "Hi, how are you?": "I'm just a bot, but I'm doing great! 😊",
-  "What are RESTful APIs?":
+  "Can you explain RESTful APIs?":
     "RESTful APIs are designed around the REST (Representational State Transfer) architecture, which uses HTTP requests to access and manipulate data. They follow a stateless, client-server, cacheable communications protocol.",
 };
 
